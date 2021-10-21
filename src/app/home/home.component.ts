324b78523c290b15ba373bf6c5cd3c760c329aaf
Andrew112 <andrew.coleman330@gmail.com>
2021-10-21 14:11:42 -0500

    1st commit Modal app

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+import { ModalService } from '../_modal';
+
+@Component({templateUrl:'home.component.html'})
+export class HomeComponent implements OnInit{
+  bodyText: string;
+
+  constructor(private modalService: ModalService){}
+
+  ngOnInit(){
+    this.bodyText ="this text can be updated in Modal1;"
+  }
+
+  openModal(id: string){
+    this.modalService.open(id);
+
+  }
+
+  closeModal(id: string){
+    this.modalService.close(id);
+  }
+}
